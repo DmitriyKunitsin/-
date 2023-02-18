48aa63a8773be985d3f26136292cc678854c9114
kunic <kunic@DESKTOP-3DUV7TD>
2023-02-18 11:25:02 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int i = 0; i <= 3; i++)
+{
+    Console.WriteLine("\t One number = " + i);
+    for (int j = 0; j <= 5; j++)
+	{
+		Console.WriteLine("\t\t Two number = " + j);
+		for (int k = 0; k <= 2; k++)
+		{
+           Console.WriteLine("\t\t\t Three number = " + k);
+        }
+	}
+	break;
+	Console.ReadLine();
+}
